e7ccdbc925ee6e97c8bea13754a642a4bf7f4301
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2021-12-13 05:49:40 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -40,5 +40,5 @@ Ticker = {
     Enabled = true, -- Decide whether the real life price ticker should be enabled or not :)  
     coin = 'ETH', 
     currency = 'USD', -- For example USD, NOK, SEK, EUR, CAD and more here https://www.countries-ofthe-world.com/world-currencies.html
-    tick_time = 5 --- Minutes (If lower than 2 minutes it will force itself to 2 minutes)
+    tick_time = 2 --- Minutes (If lower than 2 minutes it will force itself to 2 minutes)
 }
